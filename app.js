bd79281f1417e9e632b257a15ae6306bbaf29249
Arash Arabi <arasharabi.831119@gmail.com>
2021-12-11 20:02:50 +0330

    task list js project

--- app.js
+++ app.js
@@ -1,188 +1,166 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // DOM Load event
+  document.addEventListener('DOMContentLoaded', getTasks);
+  // Add task event
+  form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
+}
+
+// Get Tasks from LS
+function getTasks() {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task){
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+  });
+}
+
+// Add Task
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Store in LS
+  storeTaskInLocalStorage(taskInput.value);
+
+  // Clear input
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
+
+      // Remove from LS
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+    }
+  }
+}
+
+// Remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index){
+    if(taskItem.textContent === task){
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Clear Tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  // https://jsperf.com/innerhtml-vs-removechild
+
+  // Clear from LS
+  clearTasksFromLocalStorage();
+}
+
+// Clear Tasks from LS
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
 
-// const li  = document.createElement('li');
 
-
-// li.className = 'collection-item';
-
-
-// li.id = 'new-item';
-
-// li.setAttribute('title', 'New Item');
-
-
-// li.appendChild(document.createTextNode('Hello World'));
-
-
-// const link = document.createElement('a');
-
-// link.className = 'delete-item secondary-content';
-
-// link.innerHTML = '<i class="fa fa-remove"></i>';
-
-// li.appendChild(link);
-
-
-// document.querySelector('ul.collection').appendChild(li);
-
-// console.log(li);
-
-
-
-// const newHeading = document.createElement('h2');
-
-// newHeading.id = 'task-title';
-
-// newHeading.appendChild(document.createTextNode('Task List'));
-
-
-// const oldHeading = document.getElementById('task-title');
-
-// const cardAction = document.querySelector('.card-action');
-
-
-// cardAction.replaceChild(newHeading, oldHeading);
-
-
-// const lis = document.querySelectorAll('li');
-// const list = document.querySelector('ul');
-
-// lis[0].remove();
-
-
-// list.removeChild(lis[3]);
-
-
-// const firstLi = document.querySelector('li:first-child');
-// const link = firstLi.children[0];
-
-// let val;
-
-
-// val = link.className;
-// val = link.classList;
-// val = link.classList[0];
-// link.classList.add('test');
-// link.classList.remove('test');
-// val = link;
-
-
-// val = link.getAttribute('href');
-// val = link.setAttribute('href', 'http://google.com');
-// link.setAttribute('title', 'Google');
-// val = link.hasAttribute('title');
-// link.removeAttribute('title');
-// val = link;
-
-// console.log(val);
-
-
-
-
-// document.querySelector('.clear-tasks').addEventListener('click', onClick);
-
-// function onClick(e){
-//   //console.log('Clicked');
-
-//   let val;
-
-//   val = e;
-
-  
-//   val = e.target;
-//   val = e.target.id;
-//   val = e.target.className;
-//   val = e.target.classList;
-
-  
-//   val = e.type;
-
-  
-//   val = e.timeStamp;
-
-  
-//   val = e.clientY;
-//   val = e.clientX;
-
-  
-//   val = e.offsetY;
-//   val = e.offsetX;
-
-//   console.log(val);
-// }
-
-// const clearBtn = document.querySelector('.clear-tasks');
-// const card = document.querySelector('.card');
-// const heading = document.querySelector('h5');
-
-// // Click
-// // clearBtn.addEventListener('click', runEvent);
-// // Doubleclick
-// // clearBtn.addEventListener('dblclick', runEvent);
-// // Mousedown
-// // clearBtn.addEventListener('mousedown', runEvent);
-// // Mouseup
-// // clearBtn.addEventListener('mouseup', runEvent);
-// // Mouseenter
-// // card.addEventListener('mouseenter', runEvent);
-// // Mouseleave
-// // card.addEventListener('mouseleave', runEvent);
-// // Mouseover
-// // card.addEventListener('mouseover', runEvent);
-// // Mouseout
-// // card.addEventListener('mouseout', runEvent);
-// // Mousemove
-// card.addEventListener('mousemove', runEvent);
-
-
-// function runEvent(e) {
-//   console.log(`EVENT TYPE: ${e.type}`);
-
-//   heading.textContent= `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
-
-//   document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
-// }
-
-
-
-
-// const form = document.querySelector('form');
-// const taskInput = document.getElementById('task');
-// const heading = document.querySelector('h5');
-// const select = document.querySelector('select');
-
-
-// taskInput.value = '';
-
-// // form.addEventListener('submit', runEvent);
-
-// // Keydown
-// //taskInput.addEventListener('keydown', runEvent);
-// // Keydown
-// // taskInput.addEventListener('keyup', runEvent);
-// // Keypress
-// // taskInput.addEventListener('keypress', runEvent);
-// // Focus
-// // taskInput.addEventListener('focus', runEvent);
-// // Blur
-// // taskInput.addEventListener('blur', runEvent);
-// // Cut
-// // taskInput.addEventListener('cut', runEvent);
-// // Paste
-// // taskInput.addEventListener('paste', runEvent);
-// // Input
-// // taskInput.addEventListener('input', runEvent);
-// // Change
-// select.addEventListener('change', runEvent);
-
-// function runEvent(e){
-//   console.log(`EVENT TYPE: ${e.type}`);
-
-//   //console.log(e.target.value);
-
-//   // heading.innerText = e.target.value;
-
-//   // Get input value
-//   // console.log(taskInput.value);
-
-//   // e.preventDefault();
-// }
