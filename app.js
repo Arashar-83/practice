9d48bd84072ab98024a09afbde475f89f2656060
Arash Arabi <arasharabi.831119@gmail.com>
2021-12-22 17:17:54 +0330

    xhr req

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
-// const bookexp = {
-//     exp: function(){
-//         return `read ${this.name} I teache you ${this.sub}`
-//     }
-// }
+document.getElementById('button').addEventListener('click', loadP)
 
-// const tech = Object.create(bookexp)
-// tech.name = 'tech book'
-// tech.sub = 'technology'
-
-// const his = Object.create(bookexp)
-// his.name = 'history book'
-// his.sub = 'abute the past'
-
-// console.log(tech.exp())
-// console.log(his.exp())
-
-class phone{
-    constructor(name, company, model, color){
-        this.name = name
-        this.company = company
-        this.model = model
-        this.color = color
+function loadP(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data.txt', true);
+    xhr.onload = function(){
+        if(this.status === 200){
+            document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+        }
+        if(this.status === 404){
+            alert('file does not found')
+        }
     }
-    define(){
-        return `${this.name} is a phone of ${this.company} company and was invented in ${this.model} and it has several colors such as ${this.color}`
-    }
+    xhr.send()
 }
-
-// const iphoneX = new phone('IphoneX','apple','2020','black')
-
-// console.log(iphoneX.define())
-
-class tablet extends phone{
-    constructor(name, company, model, color, size){
-        super(name,company,model,color)
-        this.size = size
-    }
-}
-
-const lenovo = new tablet('lenovo','lenovo','69','blue','356')
-console.log(lenovo.define())
